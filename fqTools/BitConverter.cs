a8c27659967be9e4c4d0667a60ce0f72d081bae7
fuquyama <fuquyama@gmail.com>
2022-03-26 20:43:54 +0900

    Endian を指定する BitConverter を追加

---
+++ fqTools/BitConverter.cs
@@ -0,0 +1,307 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fqTools
+{
+    public static class BitConverter
+    {
+        /// <inheritdoc cref="System.BitConverter.GetBytes(short)"/>
+        /// <param name="bigEndian">BigEndianの並びにする</param>
+        public static byte[] GetBytes(short value, bool bigEndian = true)
+        {
+            if ((bigEndian && System.BitConverter.IsLittleEndian)
+                || (!bigEndian && !System.BitConverter.IsLittleEndian))
+            {
+                return System.BitConverter.GetBytes(value).Reverse().ToArray();
+            }
+            else
+            {
+                return System.BitConverter.GetBytes(value);
+            }
+        }
+
+
+        /// <inheritdoc cref="System.BitConverter.GetBytes(ushort)"/>
+        /// <param name="bigEndian">BigEndianの並びにする</param>
+        public static byte[] GetBytes(ushort value, bool bigEndian = true)
+        {
+            if ((bigEndian && System.BitConverter.IsLittleEndian)
+                || (!bigEndian && !System.BitConverter.IsLittleEndian))
+            {
+                return System.BitConverter.GetBytes(value).Reverse().ToArray();
+            }
+            else
+            {
+                return System.BitConverter.GetBytes(value);
+            }
+        }
+
+
+        /// <inheritdoc cref="System.BitConverter.GetBytes(Int32)"/>
+        /// <param name="bigEndian">BigEndianの並びにする</param>
+        public static byte[] GetBytes(Int32 value, bool bigEndian = true)
+        {
+            if ((bigEndian && System.BitConverter.IsLittleEndian)
+                || (!bigEndian && !System.BitConverter.IsLittleEndian))
+            {
+                return System.BitConverter.GetBytes(value).Reverse().ToArray();
+            }
+            else
+            {
+                return System.BitConverter.GetBytes(value);
+            }
+        }
+
+
+        /// <inheritdoc cref="System.BitConverter.GetBytes(UInt32)"/>
+        /// <param name="bigEndian">BigEndianの並びにする</param>
+        public static byte[] GetBytes(UInt32 value, bool bigEndian = true)
+        {
+            if ((bigEndian && System.BitConverter.IsLittleEndian)
+                || (!bigEndian && !System.BitConverter.IsLittleEndian))
+            {
+                return System.BitConverter.GetBytes(value).Reverse().ToArray();
+            }
+            else
+            {
+                return System.BitConverter.GetBytes(value);
+            }
+        }
+
+
+        /// <inheritdoc cref="System.BitConverter.GetBytes(Int64)"/>
+        /// <param name="bigEndian">BigEndianの並びにする</param>
+        public static byte[] GetBytes(Int64 value, bool bigEndian = true)
+        {
+            if ((bigEndian && System.BitConverter.IsLittleEndian)
+                || (!bigEndian && !System.BitConverter.IsLittleEndian))
+            {
+                return System.BitConverter.GetBytes(value).Reverse().ToArray();
+            }
+            else
+            {
+                return System.BitConverter.GetBytes(value);
+            }
+        }
+
+
+        /// <inheritdoc cref="System.BitConverter.GetBytes(UInt64)"/>
+        /// <param name="bigEndian">BigEndianの並びにする</param>
+        public static byte[] GetBytes(UInt64 value, bool bigEndian = true)
+        {
+            if ((bigEndian && System.BitConverter.IsLittleEndian)
+                || (!bigEndian && !System.BitConverter.IsLittleEndian))
+            {
+                return System.BitConverter.GetBytes(value).Reverse().ToArray();
+            }
+            else
+            {
+                return System.BitConverter.GetBytes(value);
+            }
+        }
+
+
+        /// <inheritdoc cref="System.BitConverter.GetBytes(float)"/>
+        /// <param name="bigEndian">BigEndianの並びにする</param>
+        public static byte[] GetBytes(float value, bool bigEndian = true)
+        {
+            if ((bigEndian && System.BitConverter.IsLittleEndian)
+                || (!bigEndian && !System.BitConverter.IsLittleEndian))
+            {
+                return System.BitConverter.GetBytes(value).Reverse().ToArray();
+            }
+            else
+            {
+                return System.BitConverter.GetBytes(value);
+            }
+        }
+
+
+        /// <inheritdoc cref="System.BitConverter.GetBytes(double)"/>
+        /// <param name="bigEndian">BigEndianの並びにする</param>
+        public static byte[] GetBytes(double value, bool bigEndian = true)
+        {
+            if ((bigEndian && System.BitConverter.IsLittleEndian)
+                || (!bigEndian && !System.BitConverter.IsLittleEndian))
+            {
+                return System.BitConverter.GetBytes(value).Reverse().ToArray();
+            }
+            else
+            {
+                return System.BitConverter.GetBytes(value);
+            }
+        }
+
+
+
+        /// <inheritdoc cref="System.BitConverter.ToInt16"/>
+        /// <param name="bigEndian">valueがBigEndianの並び</param>
+        public static short ToInt16(byte[] value, int startIndex, bool bigEndian = true)
+        {
+            if (value.Length - startIndex < 2)
+            {
+                throw new ArgumentOutOfRangeException("value.Length - startIndex < 2");
+            }
+            if ((bigEndian && System.BitConverter.IsLittleEndian)
+                || (!bigEndian && !System.BitConverter.IsLittleEndian))
+            {
+                byte[] rev = new byte[] { value[startIndex + 1], value[startIndex] };
+                return System.BitConverter.ToInt16(rev, 0);
+            }
+            else
+            {
+                return System.BitConverter.ToInt16(value, startIndex);
+            }
+        }
+
+
+        /// <inheritdoc cref="System.BitConverter.ToUInt16"/>
+        /// <param name="bigEndian">valueがBigEndianの並び</param>
+        public static ushort ToUInt16(byte[] value, int startIndex, bool bigEndian = true)
+        {
+            if (value.Length - startIndex < 2)
+            {
+                throw new ArgumentOutOfRangeException("value.Length - startIndex < 2");
+            }
+            if ((bigEndian && System.BitConverter.IsLittleEndian)
+                || (!bigEndian && !System.BitConverter.IsLittleEndian))
+            {
+                byte[] rev = new byte[] { value[startIndex + 1], value[startIndex] };
+                return System.BitConverter.ToUInt16(rev, 0);
+            }
+            else
+            {
+                return System.BitConverter.ToUInt16(value, startIndex);
+            }
+        }
+
+
+        /// <inheritdoc cref="System.BitConverter.ToInt32"/>
+        /// <param name="bigEndian">valueがBigEndianの並び</param>
+        public static Int32 ToInt32(byte[] value, int startIndex, bool bigEndian = true)
+        {
+            if (value.Length - startIndex < 4)
+            {
+                throw new ArgumentOutOfRangeException("value.Length - startIndex < 4");
+            }
+            if ((bigEndian && System.BitConverter.IsLittleEndian)
+                || (!bigEndian && !System.BitConverter.IsLittleEndian))
+            {
+                byte[] rev = new byte[] { value[startIndex + 3], value[startIndex + 2], value[startIndex + 1], value[startIndex] };
+                return System.BitConverter.ToInt32(rev, 0);
+            }
+            else
+            {
+                return System.BitConverter.ToInt32(value, startIndex);
+            }
+        }
+
+
+        /// <inheritdoc cref="System.BitConverter.ToUInt32"/>
+        /// <param name="bigEndian">valueがBigEndianの並び</param>
+        public static UInt32 ToUInt32(byte[] value, int startIndex, bool bigEndian = true)
+        {
+            if (value.Length - startIndex < 4)
+            {
+                throw new ArgumentOutOfRangeException("value.Length - startIndex < 4");
+            }
+            if ((bigEndian && System.BitConverter.IsLittleEndian)
+                || (!bigEndian && !System.BitConverter.IsLittleEndian))
+            {
+                byte[] rev = new byte[] { value[startIndex + 3], value[startIndex + 2], value[startIndex + 1], value[startIndex] };
+                return System.BitConverter.ToUInt32(rev, 0);
+            }
+            else
+            {
+                return System.BitConverter.ToUInt32(value, startIndex);
+            }
+        }
+
+
+        /// <inheritdoc cref="System.BitConverter.ToSingle"/>
+        /// <param name="bigEndian">valueがBigEndianの並び</param>
+        public static float ToSingle(byte[] value, int startIndex, bool bigEndian = true)
+        {
+            if (value.Length - startIndex < 4)
+            {
+                throw new ArgumentOutOfRangeException("value.Length - startIndex < 4");
+            }
+            if ((bigEndian && System.BitConverter.IsLittleEndian)
+                || (!bigEndian && !System.BitConverter.IsLittleEndian))
+            {
+                byte[] rev = new byte[] { value[startIndex + 3], value[startIndex + 2], value[startIndex + 1], value[startIndex] };
+                return System.BitConverter.ToSingle(rev, 0);
+            }
+            else
+            {
+                return System.BitConverter.ToSingle(value, startIndex);
+            }
+        }
+
+
+        /// <inheritdoc cref="System.BitConverter.ToInt64"/>
+        /// <param name="bigEndian">valueがBigEndianの並び</param>
+        public static Int64 ToInt64(byte[] value, int startIndex, bool bigEndian = true)
+        {
+            if (value.Length - startIndex < 8)
+            {
+                throw new ArgumentOutOfRangeException("value.Length - startIndex < 8");
+            }
+            if ((bigEndian && System.BitConverter.IsLittleEndian)
+                || (!bigEndian && !System.BitConverter.IsLittleEndian))
+            {
+                byte[] rev = new byte[] { value[startIndex + 7], value[startIndex + 6], value[startIndex + 5], value[startIndex + 4], value[startIndex + 3], value[startIndex + 2], value[startIndex + 1], value[startIndex] };
+                return System.BitConverter.ToInt64(rev, 0);
+            }
+            else
+            {
+                return System.BitConverter.ToInt64(value, startIndex);
+            }
+        }
+
+
+        /// <inheritdoc cref="System.BitConverter.ToUInt64"/>
+        /// <param name="bigEndian">valueがBigEndianの並び</param>
+        public static UInt64 ToUInt64(byte[] value, int startIndex, bool bigEndian = true)
+        {
+            if (value.Length - startIndex < 8)
+            {
+                throw new ArgumentOutOfRangeException("value.Length - startIndex < 8");
+            }
+            if ((bigEndian && System.BitConverter.IsLittleEndian)
+                || (!bigEndian && !System.BitConverter.IsLittleEndian))
+            {
+                byte[] rev = new byte[] { value[startIndex + 7], value[startIndex + 6], value[startIndex + 5], value[startIndex + 4], value[startIndex + 3], value[startIndex + 2], value[startIndex + 1], value[startIndex] };
+                return System.BitConverter.ToUInt64(rev, 0);
+            }
+            else
+            {
+                return System.BitConverter.ToUInt64(value, startIndex);
+            }
+        }
+
+
+        /// <inheritdoc cref="System.BitConverter.ToDouble"/>
+        /// <param name="bigEndian">valueがBigEndianの並び</param>
+        public static double ToDouble(byte[] value, int startIndex, bool bigEndian = true)
+        {
+            if (value.Length - startIndex < 8)
+            {
+                throw new ArgumentOutOfRangeException("value.Length - startIndex < 8");
+            }
+            if ((bigEndian && System.BitConverter.IsLittleEndian)
+                || (!bigEndian && !System.BitConverter.IsLittleEndian))
+            {
+                byte[] rev = new byte[] { value[startIndex + 7], value[startIndex + 6], value[startIndex + 5], value[startIndex + 4], value[startIndex + 3], value[startIndex + 2], value[startIndex + 1], value[startIndex] };
+                return System.BitConverter.ToDouble(rev, 0);
+            }
+            else
+            {
+                return System.BitConverter.ToDouble(value, startIndex);
+            }
+        }
+    }
+}
---
+++ fqToolsTests/BitConverterTests.cs
@@ -0,0 +1,190 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using fqTools;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fqTools.Tests
+{
+    [TestClass()]
+    public class BitConverterTests
+    {
+        [TestMethod()]
+        public void GetBytesTestShort()
+        {
+            byte[] expected = new byte[] { 0xCF, 0xC7 };
+            short value = -12345;
+            byte[] actual;
+            actual = BitConverter.GetBytes(value, true);
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void GetBytesTestUShort()
+        {
+            byte[] expected = new byte[] { 0xCF, 0xC7 };
+            ushort value = 53191;
+            byte[] actual;
+            actual = BitConverter.GetBytes(value, true);
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void GetBytesTestInt32()
+        {
+            byte[] expected = new byte[] { 0xF8, 0xA4, 0x32, 0xEB };
+            Int32 value = -123456789;
+            byte[] actual;
+            actual = BitConverter.GetBytes(value, true);
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void GetBytesTestUInt32()
+        {
+            byte[] expected = new byte[] { 0x7, 0x5B, 0xCD, 0x15 };
+            UInt32 value = 123456789;
+            byte[] actual;
+            actual = BitConverter.GetBytes(value, true);
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void GetBytesTestInt64()
+        {
+            byte[] expected = new byte[] { 0xFF, 0xFF, 0xFF, 0xFD, 0xB3, 0x4F, 0xE9, 0x16 };
+            Int64 value = -9876543210;
+            byte[] actual;
+            actual = BitConverter.GetBytes(value, true);
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void GetBytesTestUInt64()
+        {
+            byte[] expected = new byte[] { 0x00, 0x00, 0x00, 0x2, 0x4C, 0xB0, 0x16, 0xEA };
+            UInt64 value = 9876543210;
+            byte[] actual;
+            actual = BitConverter.GetBytes(value, true);
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void GetBytesTestSingle()
+        {
+            byte[] expected = new byte[] { 0x3B, 0x80, 0x00, 0x00 };
+            float value = 3.9062500E-003F;
+            byte[] actual;
+            actual = BitConverter.GetBytes(value, true);
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void GetBytesTestDouble()
+        {
+            byte[] expected = new byte[] { 0x01, 0xAA, 0x74, 0xFE, 0x1C, 0x1E, 0x88, 0xDF };
+            double value = 1.2345678901234500E-300;
+            byte[] actual;
+            actual = BitConverter.GetBytes(value, true);
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void ToInt16Test()
+        {
+            byte[] value = new byte[] { 0xCF, 0xC7 };
+            short expected = -12345;
+            short actual;
+            actual = BitConverter.ToInt16(value, 0, true);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void ToUInt16Test()
+        {
+            byte[] value = new byte[] { 0xCF, 0xC7 };
+            ushort expected = 53191;
+            ushort actual;
+            actual = BitConverter.ToUInt16(value, 0, true);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void ToInt32Test()
+        {
+            byte[] value = new byte[] { 0xF8, 0xA4, 0x32, 0xEB };
+            Int32 expected = -123456789;
+            Int32 actual;
+            actual = BitConverter.ToInt32(value, 0, true);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void ToUInt32Test()
+        {
+            byte[] value = new byte[] { 0x7, 0x5B, 0xCD, 0x15 };
+            UInt32 expected = 123456789;
+            UInt32 actual;
+            actual = BitConverter.ToUInt32(value, 0, true);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void ToSingleTest()
+        {
+            byte[] value = new byte[] { 0x3B, 0x80, 0x00, 0x00 };
+            float expected = 3.9062500E-003F;
+            float actual;
+            actual = BitConverter.ToSingle(value, 0, true);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void ToInt64Test()
+        {
+            byte[] value = new byte[] { 0xFF, 0xFF, 0xFF, 0xFD, 0xB3, 0x4F, 0xE9, 0x16 };
+            Int64 expected = -9876543210;
+            Int64 actual;
+            actual = BitConverter.ToInt64(value, 0, true);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void ToUInt64Test()
+        {
+            byte[] value = new byte[] { 0x00, 0x00, 0x00, 0x2, 0x4C, 0xB0, 0x16, 0xEA };
+            UInt64 expected = 9876543210;
+            UInt64 actual;
+            actual = BitConverter.ToUInt64(value, 0, true);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void ToDoubleTest()
+        {
+            byte[] value = new byte[] { 0x01, 0xAA, 0x74, 0xFE, 0x1C, 0x1E, 0x88, 0xDF };
+            double expected = 1.2345678901234500E-300;
+            double actual;
+            actual = BitConverter.ToDouble(value, 0, true);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
